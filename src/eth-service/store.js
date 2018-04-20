079a5ff0fd229f754d0251378dfcba0de6a11a78
alextanhongpin <alextan@seekasia.com>
2018-04-20 16:35:07 +0800

    chore: add new store logic

--- src/eth-service/store.js
+++ src/eth-service/store.js
@@ -9,25 +9,119 @@
 **/
 
 export default function Store (db) {
-  // When calling external services, prefix the function with fetch
-  // e.g. When calling food service
-  // async function fetchFood () {
-  //  return request('http://localhost:3000/foods/1')
-  // }
 
-  // one, all, create, update, delete, count, search is reserved for DB only operation
-  async function getBlock ({ number }) {
-    return null
+  // NOTE: In large scale projects, it might be more sensible to separate the read/write at the 
+  // code level, as well as the database user
+
+  // Reads
+
+  async function getBlockFromWeb3 ({ number }) {
+    return web3.eth.getBlock(number)
   }
  
   async function getTransaction ({ hash }) {
-    return null
+    return web3.eth.getTransaction(hash)
   }
  
   async function getTransactionReceipt ({ hash }) {
-    return null
+    return web3.eth.getTransactionReceipt(hash)
+  }
+
+  async function getBlockFromStorage ({ number }) {
+    const [rows] = await db.query('SELECT * FROM block WHERE blockNumber = ?', [number])
+    return (rows && rows[0]) || null
+  }
+
+  async function getTransactionFromStorage ({ hash }) {
+    const [rows] = await db.query('SELECT * FROM transaction WHERE transactionHash = ?', [hash])
+    return (rows && rows[0]) || null
+  }
+
+  async function getTransactionReceiptFromStorage ({ hash }) {
+    const [rows] = await db.query('SELECT * FROM transactionReceipt WHERE transactionHash = ?', [hash])
+    return (rows && rows[0]) || null
+  }
+
+  // Writes
+
+  async function postBlock (block) {
+    const [ rows ] = await db.query('INSERT INTO block (a, b) VALUES (?, ?)', ['1', '2'])
+    return rows
+  }
+
+  async function postTransaction (block) {
+    const [ rows ] = await db.query('INSERT INTO transaction (a, b) VALUES (?, ?)', ['1', '2'])
+    return rows
+  }
+
+  async function postTransactionReceipt (block) {
+    const [ rows ] = await db.query('INSERT INTO transactionReceipt (a, b) VALUES (?, ?)', ['1', '2'])
+    return rows
+  } 
+
+  // Facade operation
+
+  async function getBlock ({ number }) {
+    // Query db/cache to check if block exists
+    const block = await getBlockFromStorage({ number })
+
+    // Block is not available in current storage, query from main net
+    if (!block) {
+      const block = await getBlockFromWeb3({ number })
+
+      // Block still does not exist, throw error
+      if (!block) {
+        throw new Error(`block ${number} does not exist`)
+      }
+
+      // Store new block in the database
+      const newBlockWithId = await postBlock(block)
+      return newBlockWithId
+    }
+    return block
+  }
+
+  async function getTransaction ({ hash }) {
+    // Query db/cache to check if block exists
+    const transaction = await getTransactionFromStorage({ hash })
+
+    // Block is not available in current storage, query from main net
+    if (!transaction) {
+      const transaction = await getTransactionFromWeb3({ hash })
+
+      // Block still does not exist, throw error
+      if (!transaction) {
+        throw new Error(`transaction ${hash} does not exist`)
+      }
+
+      // Store new block in the database
+      const newTransactionWithId = await postBlock(transaction)
+      return newTransactionWithId
+    }
+    return transaction
+  }
+
+  async function getTransactionReceipt ({ hash }) {
+    // Query db/cache to check if block exists
+    const transactionReceipt = await getTransactionReceiptFromStorage({ hash })
+
+    // Block is not available in current storage, query from main net
+    if (!transactionReceipt) {
+      const transactionReceipt = await getTransactionReceiptFromWeb3({ hash })
+
+      // Block still does not exist, throw error
+      if (!transactionReceipt) {
+        throw new Error(`transaction ${hash} does not exist`)
+      }
+
+      // Store new block in the database
+      const newTransactionReceiptWithId = await postBlock(transactionReceipt)
+      return newTransactionReceiptWithId
+    }
+    return transactionReceipt
   }
  
+  // We only expose the APIs we need
   return {
     getBlock,
     getTransaction,
